7053173f064af84146940621abd457d80f113dde
Duy Tran <tranduydn@gmail.com>
2023-11-23 22:03:56 +0700

    fix testing

--- src/articles/articles.service.spec.ts
+++ src/articles/articles.service.spec.ts
@@ -1,12 +1,16 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { ArticlesService } from './articles.service';
+import { PrismaService } from '@/prisma/prisma.service';
+
+
+
 
 describe('ArticlesService', () => {
   let service: ArticlesService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
-      providers: [ArticlesService],
+      providers: [ArticlesService, { provide: PrismaService, useValue: {} }],
     }).compile();
 
     service = module.get<ArticlesService>(ArticlesService);
--- src/articles/articles.service.ts
+++ src/articles/articles.service.ts
@@ -1,11 +1,12 @@
 import { Injectable } from '@nestjs/common';
-import { PrismaService } from 'src/prisma/prisma.service';
 import { CreateArticleDto } from './dto/create-article.dto';
 import { UpdateArticleDto } from './dto/update-article.dto';
+import { PrismaService } from '@/prisma/prisma.service';
+
 
 @Injectable()
 export class ArticlesService {
-  constructor(private prisma: PrismaService) {}
+  constructor(private prisma: PrismaService) { }
 
   create(createArticleDto: CreateArticleDto) {
     return this.prisma.article.create({ data: createArticleDto });
